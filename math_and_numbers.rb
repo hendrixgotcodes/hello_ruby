b86aef74bbaeb9e3717b69f412e892c853790fa3
Samuel O. Asare <asare11samuel@gmail.com>
2023-04-27 04:37:02 +0000

    math and numbers

---
+++ math_and_numbers.rb
@@ -0,0 +1,27 @@
+puts 2**3 == 2^3
+puts 2**3
+puts 2 ^ 3
+puts 2%3
+
+num = 2
+
+# get absolute value of a number
+puts num.abs()
+
+num = 20.687
+puts num.round()
+puts num.floor()
+puts num.ceil()
+
+# convert to string
+num = 2.to_s
+puts num.is_a? String
+
+
+# Math operations
+
+# square root
+puts Math.sqrt(36)
+
+# logarithm
+Math.log(12,2)
