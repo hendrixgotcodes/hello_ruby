aacdddf6ff56ad0835d7b372e72203ab9521325b
Samuel O. Asare <asare11samuel@gmail.com>
2023-04-27 22:23:52 +0000

    madlibs

---
+++ madlibs.rb
@@ -0,0 +1,10 @@
+puts "Enter a color"
+color = gets.chomp()
+puts "Enter a plural noun"
+plural_noun = gets.chomp()
+puts "Enter a celebrity"
+celebrity_name = gets.chomp()
+
+puts "Roses are " + color
+puts "Violets are " + plural_noun
+puts "I love " + celebrity_name
