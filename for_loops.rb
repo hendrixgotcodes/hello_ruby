5d544ce4b3ff77f964ee41fd30f8f7408291cfb6
Samuel O. Asare <asare11samuel@gmail.com>
2023-04-28 00:31:26 +0000

    for loops

---
+++ for_loops.rb
@@ -0,0 +1,17 @@
+friends = ["Kevin","Adam","Oscar"]
+
+for friend in friends
+    puts friend
+end
+
+friends.each do |friend|
+    puts friend
+end
+
+for idx in 0..5
+    puts idx
+end
+
+6.times do |time|
+    puts time
+end
